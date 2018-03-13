7fad828e332b8985e5513f70ee63d3ccf7f53b73
Yury Zavarin <yury.zavarin@gmail.com>
2018-03-13 04:54:25 +0300

    Trying ktor locations feature.

--- backend/src/main/kotlin/com/github/tadam/tcbuildreport/backend/Main.kt
+++ backend/src/main/kotlin/com/github/tadam/tcbuildreport/backend/Main.kt
@@ -3,14 +3,19 @@ package com.github.tadam.tcbuildreport.backend
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.gson
+import io.ktor.locations.*
 import io.ktor.response.*
 import io.ktor.routing.*
+
+@Location("/api/ping")
+class Ping()
 
 data class Pong(val data: String)
 
 fun Application.main() {
     install(DefaultHeaders)
     install(Compression)
+    install(Locations)
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
@@ -18,7 +23,7 @@ fun Application.main() {
     }
 
     routing {
-        get("/api/ping") {
+        get<Ping> {
             call.respond(Pong("pong"))
         }
     }
